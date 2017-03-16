8059c88d4a07ec7142bcc48cd2b0c93dd87787e1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-16 12:03:38 -0700

    Initial commit

---
+++ static/coffee.js
@@ -0,0 +1,13 @@
+// Set these variables to their DOM elements
+
+var myImage;
+var myRevenueMessage;
+
+// use this JavaScript object in further study step
+
+var prices = {
+    "brew": 2.05,
+    "espresso": 3.25,
+    "latte": 4.15,
+    "hot chocolate": 2.50
+};

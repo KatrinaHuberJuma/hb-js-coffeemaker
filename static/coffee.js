3960d89496dbcc30a6dc3cf478493f1229f89f52
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-16 13:01:39 -0700

    Finished coffee making and ordering functionality

--- static/coffee.js
+++ static/coffee.js
@@ -1,7 +1,46 @@
 // Set these variables to their DOM elements
 
-var myImage;
-var myRevenueMessage;
+var myImage = document.getElementById("coffee-image");
+myImage.setAttribute("src", "static/coffee_beans.jpg")
+
+var espresso = document.getElementById("espresso");
+var latte = document.getElementById("latte")
+var hotChocolate = document.getElementById("hot-chocolate")
+
+var myRevenueMessage = document.getElementById("revenue");
+myRevenueMessage.innerHTML = "$" + 0;
+
+var drinksOrdered = [];
+
+function revenue(drinksOrdered, prices) {
+    var total = 0;
+    for (var drink of drinksOrdered) {
+        total += prices[drink];
+    }
+    // return total;
+    myRevenueMessage.innerHTML = "$" + total
+}
+
+function makeCoffee(drinkType) {
+    drinksOrdered.push(drinkType);
+    console.log(drinksOrdered);
+    myImage.setAttribute("src", "static/coffee_machine.jpg");
+    setTimeout(function () {
+        myImage.setAttribute("src", "static/coffee_mug.jpg")
+        }, 1500);
+    setTimeout(function () {
+        myImage.setAttribute("src", "static/coffee_beans.jpg")
+        }, 4000);
+    // myRevenueMessage.innerHTML = "$" + revenue(drinksOrdered, prices);
+    revenue(drinksOrdered, prices);
+}
+
+myImage.addEventListener('click', function () { return makeCoffee("brew")}); // this is like super in py classes, threfore return even when not needed
+
+espresso.addEventListener('click', function () { return makeCoffee("espresso")});
+latte.addEventListener('click', function () { return makeCoffee("latte")});
+hotChocolate.addEventListener('click', function () { return makeCoffee("hot chocolate")});
+
 
 // use this JavaScript object in further study step
 
